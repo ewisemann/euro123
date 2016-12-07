081e22e0ac8d6cfe7e0b1c4ea1dd24bd25557192
Elias Wiseman <ewiseman@yandex.com>
2016-12-07 18:54:56 -0500

    wednesday

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -14,7 +14,7 @@ class ToolsController < ApplicationController
 
 	def new
 		@tool=Tool.new
-		@toolcategories=Toolcategory.all.map{|tc| [tc.level1.to_s + " > " + tc.level2.to_s + " > " + tc.level3.to_s, tc.id] }
+		@toolcategories=Toolcategory.all.order("level1, level2, level3").map{|tc| [tc.level1.to_s + " > " + tc.level2.to_s + " > " + tc.level3.to_s, tc.id] }
 	end
 
 	def create
@@ -28,10 +28,11 @@ class ToolsController < ApplicationController
 	end
 
 	def edit
+		@toolcategories=Toolcategory.all.order("level1, level2, level3").map{|tc| [tc.level1.to_s + " > " + tc.level2.to_s + " > " + tc.level3.to_s, tc.id] }
 	end
 
 	def update
-		
+		@tool.toolcategory_id=params[:toolcategory_id]
 		if @tool.update(tool_params)
 			redirect_to tools_path
 		else
